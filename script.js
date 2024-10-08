e8142057980cce9b7b3c545f7fc7c084521c462a
geooooo <geo97it@gmail.com>
2024-10-08 16:41:44 +0300

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+const catWidth = 100;
+let isMoveRight = true;
+
+window.onload = () => {
+    const cat = window.document.querySelector(".cat");
+
+    start(cat);
+};
+
+function start(cat) {
+    const delay = 30;
+
+    cat.style.left = "10px";
+
+    setInterval(move, delay, cat);
+}
+
+function move(cat) {
+    const step = 5;
+    const left = parseInt(cat.style.left);
+    const windowWidth = window.innerWidth;
+    
+    const isRightEdge = left + step >= windowWidth - catWidth;
+    const isLeftEdge = left <= step;
+    if (isRightEdge) {
+        isMoveRight = false;
+        cat.style.transform = "rotateY(180deg)";
+    } else if (!isMoveRight && isLeftEdge) {
+        cat.style.transform = "";
+        isMoveRight = true;
+    }
+    
+    if (isMoveRight) {
+        cat.style.left = `${left + step}px`;
+    } else {
+        cat.style.left = `${left - step}px`;
+    }
+}
