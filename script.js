d6ca0c0f82f0d12c186820a621cc06a34039fc9c
geooooo <geo97it@gmail.com>
2024-10-29 23:22:43 +0300

    update v2

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
-const catWidth = 300;
-let isMoveRight = true;
-
 window.onload = () => {
+    const size = 300;
     const cat = window.document.querySelector(".cat");
 
-    start(cat);
+    cat.style.left = `${window.innerWidth / 2 - size / 2}px`;
+    cat.style.top = `${(window.innerHeight / 2 - size / 2) / 3}px`;
+
+    const paramsX = { 
+        cat,
+        size,
+        timer: null,
+        x: null,
+        isMoveForward: null,
+    };
+
+    const paramsY = { 
+        cat,
+        size,
+        timer: null,
+        y: null,
+        isMoveForward: null,
+    };
+
+    window.onclick = (event) => {
+        const { clientX: x, clientY: y } = event;
+
+        paramsX.x = x;
+        paramsX.isMoveForward = parseInt(cat.style.left) <= x;
+
+        paramsY.y = y;
+        paramsY.isMoveForward = parseInt(cat.style.top) <= y;
+
+        move(paramsX, paramsY);
+    };
 };
 
-function start(cat) {
-    const delay = 30;
+function move(paramsX, paramsY) {
+    const delay = 10;
 
-    cat.style.left = "10px";
+    clearInterval(paramsX.timer);
+    clearInterval(paramsY.timer);
+    
+    paramsX.cat.transform = "";
+    paramsX.timer = null;
+    paramsY.timer = null;
 
-    setInterval(move, delay, cat);
+    paramsX.timer = setInterval(() => moveXAnimated(paramsX), delay);
+    paramsY.timer = setInterval(() => moveYAnimated(paramsY), delay);
 }
 
-function move(cat) {
+function moveXAnimated(params) {
     const step = 5;
-    const left = parseInt(cat.style.left);
-    const windowWidth = window.innerWidth;
+    const left = parseInt(params.cat.style.left);
     
-    const isRightEdge = left + step >= windowWidth - catWidth;
-    const isLeftEdge = left <= step;
-    if (isRightEdge) {
-        isMoveRight = false;
-        cat.style.transform = "scale(-1, 1)";
-    } else if (!isMoveRight && isLeftEdge) {
-        cat.style.transform = "";
-        isMoveRight = true;
-    }
-    
-    if (isMoveRight) {
-        cat.style.left = `${left + step}px`;
+    if (params.isMoveForward) {
+        if (left + step >= params.x - params.size / 2) {
+            clearInterval(params.timer);
+        } else {
+            params.cat.style.left = `${left + step}px`;
+        }
     } else {
-        cat.style.left = `${left - step}px`;
+        if (left - step <= params.x - params.size / 2) {
+            clearInterval(params.timer);
+        } else {
+            params.cat.style.left = `${left - step}px`;
+        }
+    }
+}
+
+function moveYAnimated(params) {
+    const step = 8;
+    const top = parseInt(params.cat.style.top);
+    
+    if (params.isMoveForward) {
+        if (top + step >= params.y - params.size / 2) {
+            clearInterval(params.timer);
+        } else {
+            params.cat.style.top = `${top + step}px`;
+        }
+    } else {
+        if (top - step <= params.y - params.size / 2) {
+            clearInterval(params.timer);
+        } else {
+            params.cat.style.top = `${top - step}px`;
+        }
     }
 }
